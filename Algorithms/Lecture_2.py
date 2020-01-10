e759b79508a09d96aa1207ab406f84ae367ac770
Erniess <dragon_a@rambler.ru>
2020-01-10 18:02:47 +0300

    Algorithms in Python 3 Timofey Khirianov Lecture 2

--- Algorithms/Lecture_2.py
+++ Algorithms/Lecture_2.py
@@ -1,3 +1,6 @@
+# coding: utf8
+import random
+
 # Дизъюктивная нормальная форма
 x = [0, 0, 0, 0, 1, 1, 1, 1]
 y = [0, 0, 1, 1, 0, 0, 1, 1]
@@ -56,5 +59,68 @@ print(True and object == object and True)
 print(True or object == object or True)
 
 print('\nЗакон Де Моргана')
-print(True and object == object and True)
-print(True or object == object or True)
+print(not (True or object) == (not True and not object))
+print(not (True and object) == (not object or not True))
+
+print('\nЗакон двойного отрицания')
+print(not (not object) == object)
+
+print('\nЛогические значения. Тип bool')
+flag = False
+
+N = 10
+for i in range(N):
+    flag = (i % 10 == 0) or flag
+    print(flag)
+
+flag = True
+
+N = 10
+for i in range(N):
+    flag = flag and (i % 10 == 0)
+    print(flag)
+
+print('\nВложенные и последовательные if')
+x = 6
+if x % 2 == 0:
+    print(1)
+if x % 3 == 0:
+    print(2)
+
+if x == 6 or x == 2:
+    print(6)
+
+if x % 2 == 0:
+    if x % 3 == 0:
+        print(True )
+if x % 2 == 0 and x % 3 == 0:
+    print(6)
+else:
+    print(False)
+
+print('\nКаскадные условные конструкции ')
+# A < 0 < B < 5 < C < 10 < D
+x = random.randint(-10, 20)
+if x < 0 :
+    print('A')
+elif x < 5:
+    print('B')
+elif x < 10:
+    print('C')
+elif x>= 10:
+    print('D')
+else:
+    raise Exception
+
+y = 4
+x = -4
+if y > 0:
+    if x > 0:
+        print('I')
+    else:
+        print('II')
+else:
+    if x < 0:
+        print('III')
+    else:
+        print('IV')
